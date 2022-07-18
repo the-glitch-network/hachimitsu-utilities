d5610f7068649b27e9751619d34167b9bb373865
KJP12 <kjp12.k.joy.programer12@gmail.com>
2022-07-17 23:55:50 -0500

    Prepare for publishing to Maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
 	java
 	`java-library`
+	`maven-publish`
 }
+
+val git: String by project
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_11
@@ -28,5 +31,46 @@ tasks {
 		options.encoding = "UTF-8"
 		options.isDeprecation = true
 		options.isWarnings = true
+	}
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("maven") {
+			from(components["java"])
+
+			pom {
+				name.set("Hachimitsu Utilities")
+				description.set("Various utilities for Java.")
+
+				licenses {
+					license {
+						name.set("The Apache License, Version 2.0")
+						url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+					}
+				}
+
+				issueManagement {
+					system.set("GitHub Issues")
+					url.set("https://$git/issues")
+				}
+
+				scm {
+					connection.set("scm:git:git://$git.git")
+					developerConnection.set("scm:git:ssh://$git.git")
+					url.set("https://$git")
+				}
+			}
+		}
+	}
+	repositories {
+		System.getenv("MAVEN_URL")?.let {
+			maven(it) {
+				credentials {
+					username = System.getenv("MAVEN_USERNAME")
+					password = System.getenv("MAVEN_PASSWORD")
+				}
+			}
+		}
 	}
 }
